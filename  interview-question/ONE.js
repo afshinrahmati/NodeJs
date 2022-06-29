28f3ec2895f23c152f6d751c36c88ce37ac5d820
Afshin Rahmati <a.rahmati@kianiranian.com>
2022-06-29 13:15:27 +0430

    nodejs

---
+++  interview-question/ONE.js
@@ -0,0 +1,20 @@
+// ###### What Is NodeJs ######
+let NodeJs =
+    "open-source and free platform and we can use on web-application OR web page and it is server-side paltform and run javascript code and can use on vide streamong*single page application*online chat application";
+let whatIsSerVerSide =
+    "it is about backEnd if your code run on back that mean your user not see your result code like (connect to data base) it backend and front your user see your result it name is client-side";
+
+let mongo = "it is schemaless that mean is no necessary structur for data and you can type mix on this .and it is document-oriented and it write with c++ and it outPut Json and it has alot of optional schema";
+
+let sql = "schema base ,and if our project it's greater(large)DataBase Or we have relational databases AND it is open-source*(RDMS)=>relation database management systeam<=.";
+
+let jwt = "json Web Token a standard for transmitting information";
+
+let evenLoop = "when we use async/awati ,untile my code go to run this to that time we are wati it name evenloop";
+
+let singleThread = "for example we open the chrom-goole then open a tap first create proceedings and then open threatened and we have multithreatend ‌But in node js we have single threated";
+let npm = "Node package manager*(install it for use package AND deendcey)*";
+let nodeModules = "this is a librarise for use comfortable use like jalai-moment";
+let http = "this is a modle for create server on nodejs with out frame work";
+let fs = "file systeam";
+let event = "log for connect to datatbase or create read stream";
